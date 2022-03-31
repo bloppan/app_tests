8d3dd9708335a13f045a11c05a485953a25df0f3
blpanadero <bloppan@etsid.upv.es>
2022-03-31 14:07:37 +0200

    PCA9532_test updated with led blink

--- PCA9532_test/main.c
+++ PCA9532_test/main.c
@@ -11,27 +11,29 @@
 #include <getopt.h>
 
 
+uint8_t blink_led = 0;
+
 void set_led(uint8_t led, char * color)
 {
 
-	 if(!strcmp(optarg, "GREEN")){
-		 setLED_Value(led, GREEN);
-	 }else if(!strcmp(optarg, "RED")){
-		 setLED_Value(led, RED);
-	 }else if(!strcmp(optarg, "BLUE")){
-		 setLED_Value(led, BLUE);
-	 }else if(!strcmp(optarg, "YELLOW")){
-		 setLED_Value(led, YELLOW);
-	 }else if(!strcmp(optarg, "PURPLE")){
-		 setLED_Value(led, PURPLE);
-	 }else if(!strcmp(optarg, "CYAN")){
-		 setLED_Value(led, CYAN);
-	 }else if(!strcmp(optarg, "WHITE")){
-		 setLED_Value(led, WHITE);
-	 }else if(!strcmp(optarg, "OFF")){
-		 setLED_Value(led, LED_OFF);
+	 if(!strcmp(color, "GREEN")){
+		 setLED_Color_Blink(led, GREEN, blink_led);
+	 }else if(!strcmp(color, "RED")){
+		 setLED_Color_Blink(led, RED, blink_led);
+	 }else if(!strcmp(color, "BLUE")){
+		 setLED_Color_Blink(led, BLUE, blink_led);
+	 }else if(!strcmp(color, "YELLOW")){
+		 setLED_Color_Blink(led, YELLOW, blink_led);
+	 }else if(!strcmp(color, "PURPLE")){
+		 setLED_Color_Blink(led, PURPLE, blink_led);
+	 }else if(!strcmp(color, "CYAN")){
+		 setLED_Color_Blink(led, CYAN, blink_led);
+	 }else if(!strcmp(color, "WHITE")){
+		 setLED_Color_Blink(led, WHITE, blink_led);
+	 }else if(!strcmp(color, "OFF")){
+		 setLED_Color_Blink(led, LED_OFF, blink_led);
 	 }else{
-		printf("[ERROR]\t\t[PCA9532] Invalid color \n");
+		printf("[ERROR][PCA9532]\t\tInvalid color \n");
 	 }
 
 }
@@ -43,8 +45,6 @@ int main(int argc, char *argv[])
 	char route[256] = {0};
 
 	int c;
-	int wakeup = 0;
-	int RTC_value = 0;
     int option_index = 0;
 
     static struct option long_options[] =
@@ -57,6 +57,7 @@ int main(int argc, char *argv[])
         {"LED3",	required_argument,       0, 'c'},
         {"LED4",	required_argument,       0, 'd'},
         {"ALL",		required_argument,       0, 'e'},
+        {"BLINK",	required_argument,       0, 'f'},
         {0, 0, 0, 0}
       };
 
@@ -76,7 +77,7 @@ int main(int argc, char *argv[])
 		printf("[OK]\t[PCA9532]\tLibrary %s loaded successfully \n", route);
 
 		// Carga de funciones de la libreria I2C:
-		setLED_Value = (error_type ( *)(uint8_t, uint8_t)) dlsym(libHandlerPCA9532, "setLED_Value");
+		setLED_Color_Blink = (error_type ( *)(uint8_t, uint8_t, uint8_t)) dlsym(libHandlerPCA9532, "setLED_Color_Blink");
 		PCA9532_Initialize = (error_type ( *)(void)) dlsym(libHandlerPCA9532, "PCA9532_Initialize");
 
 		// Inicializa el controlador de LEDs
@@ -107,6 +108,9 @@ int main(int argc, char *argv[])
 	            		 set_led(i, optarg);
 	            	 }
 	                 break;
+	             case 'f':
+	            	 blink_led = atoi(optarg);
+	            	 break;
 	        }
 	    }
 
@@ -115,8 +119,9 @@ int main(int argc, char *argv[])
 	    	printf("\n\n\nExample usage: \n\n");
 	    	printf("\tPCA9532_test -LED1 GREEN \n");
 	    	printf("\tPCA9532_test -LED1 OFF -LED3 RED -LED4 YELLOW \n");
-	    	printf("\tPCA9532_test -ALL WHITE \n\n\n");
-
+	    	printf("\tPCA9532_test -ALL WHITE \n");
+	    	printf("\tPCA9532_test -LED1 GREEN -BLINK 1 -LED2 BLUE -LED3 RED\n");
+	    	printf("\tPCA9532_test -LED1 GREEN -BLINK 1 -LED2 BLUE -BLINK 2 -LED3 RED\n\n\n");
 			// Test general
 			printf("TEST GENERAL \n\n");
 
@@ -124,23 +129,23 @@ int main(int argc, char *argv[])
 
 				for(int j = 0; j < 3; j++){
 
-					setLED_Value(i, j);
+					setLED_Color_Blink(i, j, 0);
 					sleep(1);
 				}
 			}
 
-			setLED_Value(LED0, PURPLE);
+			setLED_Color_Blink(LED0, PURPLE, 0);
 			sleep(1);
-			setLED_Value(LED1, CYAN);
+			setLED_Color_Blink(LED1, CYAN, 0);
 			sleep(1);
-			setLED_Value(LED2, YELLOW);
+			setLED_Color_Blink(LED2, YELLOW, 0);
 			sleep(1);
-			setLED_Value(LED3, WHITE);
+			setLED_Color_Blink(LED3, WHITE, 0);
 			sleep(1);
 
 			for(int i = 0; i < 4; i ++){
 
-				setLED_Value(i, LED_OFF);
+				setLED_Color_Blink(i, LED_OFF, 0);
 				sleep(1);
 			}
 	    }
